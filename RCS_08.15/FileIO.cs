17f329ffe506c29653156aa96d23d61236270857
RediGt <red.i@inbox.lv>
2020-08-15 11:34:36 +0300

    Add project files.

---
+++ RCS_08.15/FileIO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace RCS_08._15
+{
+    class FileIO
+    {
+        public static List<string> Read()
+        {
+            String line;
+            List<string> a = new List<string>();
+            try
+            {
+                StreamReader sr = new StreamReader("Test.txt");
+
+                line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    a.Add((line));
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+                return a;
+            }
+            catch
+            {
+                Console.WriteLine("Neizdevas atvert failu!");
+                return null;
+            }
+        }
+
+        public static void Write(List<string> a)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("Test.txt", false);
+
+                foreach (var item in a)
+                    sw.WriteLine(item);
+
+                sw.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Neizdevas ierakstit faila!");
+            }
+        }
+    }
+}
---
+++ RCS_08.15/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RCS_08._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskEx();
+        }
+
+        static void TaskEx()
+        {
+            List<string> a = new List<string>();
+
+            UserMenu(a);
+        }
+       
+        static void PrintList(List<string> a)
+        {
+            Console.WriteLine("No     | Text");
+            for (int i = 0; i < a.Count; i++)
+            {
+                Console.WriteLine("{0}      | {1}", (i+1), a[i]);
+            }
+                
+        }
+
+        static void UserMenu(List<string> a)
+        {
+            string userChoice = null;
+            while (userChoice != "Q")
+            {
+                userChoice = UserAction();
+
+                switch (userChoice)
+                {
+                    case "1":
+                        AddString(a);
+                        break;
+                    case "2":
+                        EditString(a);
+                        break;
+                    case "3":
+                        DeleteString(a);
+                        break;
+                    case "4":
+                        a = FileIO.Read();
+                        break;
+                    case "5":
+                        FileIO.Write(a);
+                        break;
+                    case "Q":
+                        FileIO.Write(a);
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect input.\n");
+                        break;
+                }
+                PrintList(a);
+            }
+        }
+
+        static string UserAction()
+        {
+            Console.Write("\nAllowed actions: ");
+            Console.WriteLine("\n1 - add string\n" +
+                "2 - edit string\n" +
+                "3 - delete string\n" +
+                "4 - load text\n" +
+                "5 - save text\n" +
+                "q - exit\n");
+            Console.Write("Make your choice: ");
+
+            return Console.ReadLine().ToUpper();
+        }
+
+        static void AddString(List<string> a)
+        {
+            Console.Write("Input string: ");
+            string textStr = Console.ReadLine();
+            a.Add(textStr);
+            Console.WriteLine();
+        }
+
+        static void EditString(List<string> a)
+        {
+            int row = InputCheck(a);
+            Console.Write("Input new string: ");
+            string textStr = Console.ReadLine();
+            a[row-1] = textStr;
+        }
+
+        static void DeleteString(List<string> a)
+        {            
+            int row = InputCheck(a);
+            a.RemoveAt(row - 1);
+        }
+
+        static int InputCheck(List<string> a)
+        {
+            bool correctInput = false;
+            string textStr;
+            do
+            {
+                Console.Write("Input number of menu string to delete / edit: ");
+                textStr = Console.ReadLine();
+
+                for (int i = 0; i < a.Count; i++)
+                {
+                    if (textStr == Convert.ToString(i + 1))
+                        correctInput = true;
+                }
+            }
+            while (!correctInput);
+            return Convert.ToInt32(textStr);
+        }
+    }
+}
